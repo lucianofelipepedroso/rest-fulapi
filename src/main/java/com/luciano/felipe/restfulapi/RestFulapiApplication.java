15825ea7e4f4918a1145723cafb88748c089f93d
Luciano Felipe Pedroso <lucianofelipepedroso@hotmail.com>
2021-01-05 14:32:21 -0300

    Initial commit

---
+++ src/main/java/com/luciano/felipe/restfulapi/RestFulapiApplication.java
@@ -0,0 +1,13 @@
+package com.luciano.felipe.restfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFulapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFulapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luciano/felipe/restfulapi/RestFulapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luciano.felipe.restfulapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFulapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
